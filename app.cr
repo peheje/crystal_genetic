ac11d66cbe1d3179c97a4692911216dbe9832934
Peter Helstrup Jensen <peterhj@gmail.com>
2020-11-14 14:53:22 +0100

    some adjustments with arguments

--- app.cr
+++ app.cr
@@ -4,7 +4,7 @@ def generate_data()
     file1 = File.new "ids1.data", "w"
     file2 = File.new "ids2.data", "w"
 
-    10_000_000.times do
+    1_000_000.times do
         id1 = UUID.random
         file1.puts id1
 
@@ -39,30 +39,32 @@ def in_only_first(xs : Array(String), ys : Set(String))
     return only_first
 end
 
-# generate_data()
+generate_data()
 # puts in_both(["a", "b", "c"], ["a", "c"].to_set)
 # puts in_only_first(["a", "b", "c", "d"], ["a", "b", "c"].to_set)
 
 start_dt = Time.utc
 
-a = File.read_lines("ids1.data")
-a_set = a.to_set
+if ARGV.size == 2
+    a = File.read_lines(ARGV[0])
+    a_set = a.to_set
 
-b = File.read_lines("ids2.data")
-b_set = b.to_set
+    b = File.read_lines(ARGV[1])
+    b_set = b.to_set
 
-end_dt = Time.utc
-puts end_dt - start_dt
+    end_dt = Time.utc
+    puts "files read time: #{end_dt - start_dt}"
 
-start_dt = Time.utc
+    start_dt = Time.utc
 
-a_and_b = in_both(a, b_set)
-only_a = in_only_first(a, b_set)
-only_b = in_only_first(b, a_set)
+    a_and_b = in_both(a, b_set)
+    only_a = in_only_first(a, b_set)
+    only_b = in_only_first(b, a_set)
 
-puts "both #{a_and_b.size}"
-puts "only a #{only_a.size}"
-puts "only b #{only_b.size}"
+    puts "both #{a_and_b.size}"
+    puts "only a #{only_a.size}"
+    puts "only b #{only_b.size}"
 
-end_dt = Time.utc
-puts end_dt - start_dt
+    end_dt = Time.utc
+    puts "compare time: #{end_dt - start_dt}"
+end
